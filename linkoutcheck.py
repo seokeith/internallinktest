cf125b6264b9139d8340c6451f219e03a7bec72e
seokeith <141214829+seokeith@users.noreply.github.com>
2023-08-02 09:43:38 +0100

    Update linkoutcheck.py

--- linkoutcheck.py
+++ linkoutcheck.py
@@ -1,46 +1,56 @@
 import streamlit as st
 import urllib.request
 from html.parser import HTMLParser
+import csv
+import pandas as pd
 
 class MyHTMLParser(HTMLParser):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.in_paragraph = False
         self.links = []
+        self.current_link = None
 
     def handle_starttag(self, tag, attrs):
         if tag == 'p':
             self.in_paragraph = True
         elif tag == 'a' and self.in_paragraph:
-            href = dict(attrs).get('href')
-            if href:
-                self.links.append(href)
+            self.current_link = dict(attrs).get('href')
 
     def handle_endtag(self, tag):
         if tag == 'p':
             self.in_paragraph = False
+        elif tag == 'a' and self.in_paragraph:
+            self.current_link = None
+
+    def handle_data(self, data):
+        if self.current_link is not None:
+            self.links.append((self.current_link, data.strip()))
 
 def get_links(urls):
-    parser = MyHTMLParser()
+    results = []
     for url in urls:
         try:
             with urllib.request.urlopen(url) as response:
-                html = response.read()
-                parser.feed(str(html))
+                html = response.read().decode('utf-8')
+                parser = MyHTMLParser()
+                parser.feed(html)
+                for link, text in parser.links:
+                    results.append((url, link, text))
         except Exception as e:
             st.error(f"Failed to retrieve data from {url}. Error: {str(e)}")
-    return parser.links
+    return results
 
 def main():
     st.title("URL Link Extractor")
-    uploaded_file = st.file_uploader("Upload a file with URLs", type='txt')
+    uploaded_file = st.file_uploader("Upload a CSV file with URLs", type='csv')
     if uploaded_file is not None:
-        text_string = uploaded_file.read().decode('utf-8')
-        urls = text_string.split("\n")
+        csv_reader = csv.reader(uploaded_file)
+        urls = [row[0] for row in csv_reader]  # Assumes URLs are in first column
         if st.button("Extract links"):
-            links = get_links(urls)
-            for link in links:
-                st.write(link)
+            data = get_links(urls)
+            df = pd.DataFrame(data, columns=['Scraped URL', 'Link URL', 'Link Text'])
+            st.table(df)
 
 if __name__ == "__main__":
     main()
